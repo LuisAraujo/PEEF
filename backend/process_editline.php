b5913c1ccdc98764d16f82e2a62aaca2e24ba283
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2020-04-25 15:36:14 -0300

    fixed the edit line comparison

--- backend/process_editline.php
+++ backend/process_editline.php
@@ -11,28 +11,39 @@ $result = $mysqli->query($query);
 
 while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
 
+    //get code compilation that not computed lines editss
     $query2 = "SELECT Compilation.id as id, CodeCompilation.id as codeid, code FROM Compilation INNER JOIN CodeCompilation ON CodeCompilation.Compilation_id = Compilation.id WHERE CodeCompilation.linesedited = 0 AND Code_id = (SELECT id FROM Code WHERE Project_id = ".$row["id"]."  LIMIT 1)";
-
     $result2 = $mysqli->query($query2);
+
+    $query4 = "SELECT Compilation.id as id, CodeCompilation.id as codeid, code FROM Compilation INNER JOIN CodeCompilation ON CodeCompilation.Compilation_id = Compilation.id WHERE CodeCompilation.linesedited = 1 AND Code_id = (SELECT id FROM Code WHERE Project_id = 1  LIMIT 1) ORDER BY CodeCompilation.id DESC LIMIT 1";
+
+    $result4 = $mysqli->query($query4);
     $oldcode = "";
 
+    if(mysqli_num_rows($result4)) {
+        $row4 = $result4->fetch_array(MYSQLI_ASSOC);
+        $oldcode = $row4["code"];
+    }
+
+
+    //for each codecompilation
     while ($row2 = $result2->fetch_array(MYSQLI_ASSOC)) {
         echo "old = " .$oldcode . "<br>";
         echo "cur = ". $row2["code"]."<br><br>";
 
-        //$lines = array( array(-1,-1) );
-
+        //computing edit line
         $lines = comparation_lines($row2["code"], $oldcode );
-
+        //for each line computed set in bds
         for( $i = 0; $i < count($lines); $i++) {
             $query3 = "INSERT INTO LineEdited VALUES(NULL, " . $lines[$i][0] . ", " . $lines[$i][1] . " ," . $row2["codeid"] . ")";
             echo $query3. "<br>";
             $result3 = $mysqli->query($query3);
         }
-
+        //update flag in CodeCompilation for true (lineedited is computade)
         $query4 = "UPDATE CodeCompilation SET linesedited = 1 WHERE  id = ".$row2["codeid"];
         $result4 = $mysqli->query($query4);
 
+        //save this code as oldcode to next interaction
         $oldcode = $row2["code"];
     }
 }
