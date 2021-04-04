7f1ec8f68b817215e332a894a8f3378cb92fe4e2
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2021-04-04 04:00:01 -0300

    fix bug in get last messages

--- backend/chat/getlastmessage.php
+++ backend/chat/getlastmessage.php
@@ -3,11 +3,11 @@
 @include "../conection_database.php";
 @include "../session/manager_section.php";
 
+$lastidmsg = -1;
 
 if(isset( $_POST["lastidmsg"]))
     $lastidmsg = $_POST["lastidmsg"];
 
-$lastidmsg = -1;
 
 $query = "SELECT * FROM message WHERE Project_id = ".getcurrentproject_session()." AND id > '".$lastidmsg."'";
 
